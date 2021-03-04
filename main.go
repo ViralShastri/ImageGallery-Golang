78f14b2e8f66f5400033300bc523e60a0753a6cd
Viral Shsatri <shastriv19@gmail.com>
2021-03-04 14:54:11 +0530

    First Web Server

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Welcome to my First Web Application in GoLang</h1>")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", handlerFunc)
+
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "<h1>Welcome to my First Web Application in GoLang</h1>")
+	// }) // Same as Above
+
+	http.ListenAndServe(":3000", nil)
 }
