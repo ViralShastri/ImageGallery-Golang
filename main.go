a6125baa4f19aad0c77213bff6eacf8e74c08ee5
Viral Shsatri <shastriv19@gmail.com>
2021-03-06 15:01:39 +0530

    Explictly using  ServerMux type

--- main.go
+++ main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// Explictly using  ServerMux type
+
+	mux := &http.ServeMux{}
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
 		if r.URL.Path == "/" {
 			fmt.Fprintf(w, "<h1>Welcome to my First Web Application in GoLang</h1>")
@@ -17,5 +21,17 @@ func main() {
 			fmt.Fprint(w, "<h1>404 Page Not Found</h1><p>We couldn't find the page you're looking for :(</p>")
 		}
 	})
-	http.ListenAndServe(":3000", nil)
+
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Header().Set("Content-Type", "text/html")
+	// 	if r.URL.Path == "/" {
+	// 		fmt.Fprintf(w, "<h1>Welcome to my First Web Application in GoLang</h1>")
+	// 	} else if r.URL.Path == "/contact-us" {
+	// 		fmt.Fprint(w, "To get in touch, please send us a email at <a href=\"mailto:support@github.com\">support@github.com</a>.")
+	// 	} else {
+	// 		w.WriteHeader(http.StatusNotFound)
+	// 		fmt.Fprint(w, "<h1>404 Page Not Found</h1><p>We couldn't find the page you're looking for :(</p>")
+	// 	}
+	// })
+	http.ListenAndServe(":3000", mux)
 }
