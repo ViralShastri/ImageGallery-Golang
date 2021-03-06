60f78c0f2cef01a94ca95b98d974b3ef621c336d
Viral Shsatri <shastriv19@gmail.com>
2021-03-06 15:17:22 +0530

    httprouter pkg

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ViralShastri/usegolang
 
 go 1.16
+
+require github.com/julienschmidt/httprouter v1.3.0
--- main.go
+++ main.go
@@ -2,24 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
-	// Explictly using  ServerMux type
 
-	mux := &http.ServeMux{}
+	router := httprouter.New()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		if r.URL.Path == "/" {
-			fmt.Fprintf(w, "<h1>Welcome to my First Web Application in GoLang</h1>")
-		} else if r.URL.Path == "/contact-us" {
-			fmt.Fprint(w, "To get in touch, please send us a email at <a href=\"mailto:support@github.com\">support@github.com</a>.")
-		} else {
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprint(w, "<h1>404 Page Not Found</h1><p>We couldn't find the page you're looking for :(</p>")
-		}
+	router.GET("/", func(rw http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		fmt.Fprint(rw, "Welcome!")
+	})
+	router.GET("/hello/:name", func(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprintf(rw, "Welcome!, %s", p.ByName("name"))
 	})
 
 	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -33,5 +30,7 @@ func main() {
 	// 		fmt.Fprint(w, "<h1>404 Page Not Found</h1><p>We couldn't find the page you're looking for :(</p>")
 	// 	}
 	// })
-	http.ListenAndServe(":3000", mux)
+
+	log.Fatal(http.ListenAndServe(":3000", router))
+
 }
