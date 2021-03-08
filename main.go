4db3fdbe25d070b98f7b5a3bec736afe05f8a9f4
Viral Shsatri <shastriv19@gmail.com>
2021-03-08 13:55:25 +0530

    Creating Views

--- main.go
+++ main.go
@@ -1,42 +1,84 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+var (
+	homeTemplate         *template.Template
+	contactTemplate      *template.Template
+	faqTemplate          *template.Template
+	pageNotFoundTemplate *template.Template
+)
+
 func pageNotFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1>404 Page Not Found</h1><p>We couldn't find the page you're looking for :(</p>")
+	// fmt.Fprint(w, "<h1>404 Page Not Found</h1><p>We couldn't find the page you're looking for :(</p>")
+	if err := pageNotFoundTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my First Web Application in GoLang</h1>")
+	// fmt.Fprint(w, "<h1>Welcome to my First Web Application in GoLang</h1>")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
+	// fmt.Fprint(w, `<ul>
+	// <li>FAQ 1</li>
+	// <li>FAQ 2</li>
+	// <li>FAQ 2</li>
+	// </ul>
+	// `)
 
-	fmt.Fprint(w, `<ul>
-	<li>FAQ 1</li>
-	<li>FAQ 2</li>
-	<li>FAQ 2</li>
-	</ul>
-	`)
+	if err := faqTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "To get in touch, please send us a email at <a href=\"mailto:support@github.com\">support@github.com</a>.")
+	// fmt.Fprint(w, "To get in touch, please send us a email at <a href=\"mailto:support@github.com\">support@github.com</a>.")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
+
 }
 
 func main() {
+
+	var err error
+
+	homeTemplate, err = template.ParseFiles("views/home.html")
+	if err != nil {
+		panic(err)
+	}
+
+	contactTemplate, err = template.ParseFiles("views/contact.html")
+	if err != nil {
+		panic(err)
+	}
+
+	faqTemplate, err = template.ParseFiles("views/faq.html")
+	if err != nil {
+		panic(err)
+	}
+
+	pageNotFoundTemplate, err = template.ParseFiles("views/404.html")
+	if err != nil {
+		panic(err)
+	}
 
 	router := mux.NewRouter()
 
@@ -44,8 +86,8 @@ func main() {
 	router.HandleFunc("/faq", faq)
 	router.HandleFunc("/contact", contact)
 
-	router.NotFoundHandler = http.HandlerFunc(pageNotFound)
 	// router.NotFoundHandler = router.NewRoute().HandlerFunc(pageNotFound).GetHandler()
+	router.NotFoundHandler = http.HandlerFunc(pageNotFound)
 
 	log.Fatal(http.ListenAndServe(":3000", router))
 
